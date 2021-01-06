09af47f19339f780138627b1c2a8c09e6a7ba266
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 14:07:25 +0900

    Fix : Refactoring  -  DI를 위해 Interface,구현체   분리

---
+++ eatgo-api/src/main/java/kr/co/eatgo/domain/RestaurantRepositoryImpl.java
@@ -0,0 +1,35 @@
+package kr.co.eatgo.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantRepositoryImpl(){
+        Restaurant restaurant1 = new Restaurant(1004L,"Bob Zip","Seoul");
+        Restaurant restaurant2 = new Restaurant(2004L,"Cyber Food","Seoul");
+        restaurants.add(restaurant1);
+        restaurants.add(restaurant2);
+
+    }
+    @Override
+    public List<Restaurant> findAll(){
+        return restaurants;
+    }
+
+    @Override
+    public Restaurant findById(Long id){
+        //리스트로 부터 특정 객체를 가져오는 방법
+        Restaurant restaurant = restaurants.stream()
+                .filter(r-> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        return restaurant;
+    }
+}
