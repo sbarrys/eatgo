5775bec55499890b43e13ae3bd5a3769815e0189
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 12:41:54 +0900

    Test : GET Restratants  - red

---
+++ eatgo-api/src/test/java/kr/co/eatgo/interfaces/RestaurantControllerTests.java
@@ -0,0 +1,22 @@
+package kr.co.eatgo.interfaces;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class) //get 을 누구한테 요청할것인가.  Spring Runner에게 요청할 것이다.
+@WebMvcTest(RestaurantController.class) // 컨트롤러 테스트한다는 것
+class RestaurantControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void list() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/restaurants")).andExpect(status().isOk());
+    }
+}
