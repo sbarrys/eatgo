6a1d3e6f70c3dbf479c2477557ae5a73e2066df3
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 12:55:24 +0900

    Test : GET Restratants  - red

--- eatgo-api/src/test/java/kr/co/eatgo/interfaces/RestaurantControllerTests.java
+++ eatgo-api/src/test/java/kr/co/eatgo/interfaces/RestaurantControllerTests.java
@@ -1,5 +1,6 @@
 package kr.co.eatgo.interfaces;
 
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) //get 을 누구한테 요청할것인가.  Spring Runner에게 요청할 것이다.
@@ -17,6 +20,13 @@ class RestaurantControllerTests {
 
     @Test
     public void list() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/restaurants")).andExpect(status().isOk());
+        mvc.perform(MockMvcRequestBuilders.get("/restaurants"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(
+                        StringContains.containsString("\"name\":\"Bob Zip\"")
+                ))
+                .andExpect(content().string(
+                        StringContains.containsString("\"id\":\"1004\"")
+                ));
     }
 }
