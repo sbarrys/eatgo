77fd698b4da62ed92df2aca27f2f342c57699f96
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 11:18:01 +0900

    Test : Restaurant get Name test - red

--- eatgo-api/src/test/java/kr/co/eatgo/domain/RestaurantTests.java
+++ eatgo-api/src/test/java/kr/co/eatgo/domain/RestaurantTests.java
@@ -1,5 +1,16 @@
-import static org.junit.Assert.*;
+package kr.co.eatgo.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import  static org.hamcrest.MatcherAssert.assertThat;
+import  static org.hamcrest.CoreMatchers.is;
 
 public class RestaurantTests {
+    @Test
+    @DisplayName("식당이름은 BobZip")
+    public void creation(){
+        Restaurant restaurant = new Restaurant();
+        assertThat(restaurant.getName(),is("Bob Zip"));
+    }
 
 }
