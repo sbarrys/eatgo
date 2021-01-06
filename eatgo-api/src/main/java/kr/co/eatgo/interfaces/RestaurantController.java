3101c0ce50c58b0fefc1a4a00257af7774ee6294
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 14:07:44 +0900

    Fix - di interface

--- eatgo-api/src/main/java/kr/co/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/eatgo/interfaces/RestaurantController.java
@@ -1,19 +1,17 @@
 package kr.co.eatgo.interfaces;
 
-import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import kr.co.eatgo.domain.Restaurant;
 import kr.co.eatgo.domain.RestaurantRepository;
+import kr.co.eatgo.domain.RestaurantRepositoryImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.geom.RectangularShape;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
-
+    //구현체를 직접 주입하는 것이 아닌 인터페이스를 주입해준다. 이것이 DI 로 부터 얻을 수 있는 장점이다.
     private RestaurantRepository restaurantRepository;
 
     public RestaurantController(RestaurantRepository restaurantRepository){
