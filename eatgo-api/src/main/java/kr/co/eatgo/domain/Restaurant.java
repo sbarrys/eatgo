9cf9b12f6c695ef4dea163672f578f3b781caf7d
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 21:36:31 +0900

    Fix : use  MenuItem

--- eatgo-api/src/main/java/kr/co/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/eatgo/domain/Restaurant.java
@@ -1,9 +1,12 @@
 package kr.co.eatgo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Restaurant {
     private final String name;
     private final String address;
     private final Long id;
+    private List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
     public Restaurant(Long id,String name, String address){
         this.id = id;
@@ -24,6 +27,18 @@ public class Restaurant {
     }
 
     public String getInformation() {
-        return name + " in " + address;
+        return name + " in " + address+menuItems;
+    }
+    public List<MenuItem> getMenuItems(){
+        return menuItems;
+    }
+    public void addMenuItem(MenuItem name) {
+        menuItems.add(name);
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        for( MenuItem menuItem: menuItems){
+            addMenuItem(menuItem);
+        }
     }
 }
--- eatgo-api/src/main/java/kr/co/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/eatgo/interfaces/RestaurantController.java
@@ -1,21 +1,22 @@
 package kr.co.eatgo.interfaces;
 
+import kr.co.eatgo.domain.MenuItem;
+import kr.co.eatgo.domain.MenuItemRepository;
 import kr.co.eatgo.domain.Restaurant;
 import kr.co.eatgo.domain.RestaurantRepository;
-import kr.co.eatgo.domain.RestaurantRepositoryImpl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 public class RestaurantController {
     //구현체를 직접 주입하는 것이 아닌 인터페이스를 주입해준다. 이것이 DI 로 부터 얻을 수 있는 장점이다.
     private RestaurantRepository restaurantRepository;
-
-    public RestaurantController(RestaurantRepository restaurantRepository){
+    private MenuItemRepository menuItemRepository;
+    public RestaurantController(RestaurantRepository restaurantRepository,MenuItemRepository menuItemRepository){
         this.restaurantRepository = restaurantRepository;
+        this.menuItemRepository = menuItemRepository;
     }
     @GetMapping("restaurants")
     public List<Restaurant> list(){
@@ -26,6 +27,9 @@ public class RestaurantController {
     @GetMapping("restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id){
         Restaurant restaurant = restaurantRepository.findById(id);
+        List<MenuItem> menuItems = menuItemRepository.findAllByRestaurantId(id);
+
+        restaurant.setMenuItems(menuItems);
         return restaurant;
     }
 }
