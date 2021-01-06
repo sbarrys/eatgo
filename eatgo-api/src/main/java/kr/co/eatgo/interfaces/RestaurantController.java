d7a65c0a665daf8411f0b89973296599380dd67e
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 13:51:12 +0900

    Fix : Refactoring  With using Repository

--- eatgo-api/src/main/java/kr/co/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/eatgo/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package kr.co.eatgo.interfaces;
 
 import ch.qos.logback.core.pattern.util.RegularEscapeUtil;
 import kr.co.eatgo.domain.Restaurant;
+import kr.co.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,27 +14,16 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository restaurantRepository= new RestaurantRepository();
     @GetMapping("restaurants")
     public List<Restaurant> list(){
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L,"Bob Zip","Seoul");
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = restaurantRepository.findAll();
         return restaurants;
     }
 
     @GetMapping("restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id){
-        List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant1 = new Restaurant(1004L,"Bob Zip","Seoul");
-        Restaurant restaurant2 = new Restaurant(2004L,"Cyber Food","Seoul");
-        restaurants.add(restaurant1);
-        restaurants.add(restaurant2);
-
-        //리스트로 부터 특정 객체를 가져오는 방법
-        Restaurant restaurant = restaurants.stream()
-                .filter(r-> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        Restaurant restaurant = restaurantRepository.findById(id);
         return restaurant;
     }
 }
