ff7526c98f4d3f52a132cdc5329147419eb0a119
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-06 11:46:21 +0900

    Test : Restaurant get Info Test  - green

--- eatgo-api/src/main/java/kr/co/eatgo/domain/Restaurant.java
+++ eatgo-api/src/main/java/kr/co/eatgo/domain/Restaurant.java
@@ -1,7 +1,18 @@
 package kr.co.eatgo.domain;
 
 public class Restaurant {
+    private final String name;
+    private final String address;
+
+    public Restaurant(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
     public String getName() {
-        return "";
+        return name;
+    }
+
+    public String getInformation() {
+        return name + " in " + address;
     }
 }
