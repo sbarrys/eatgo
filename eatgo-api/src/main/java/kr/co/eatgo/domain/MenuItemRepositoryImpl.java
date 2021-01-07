a27387d7edd70d1560c79e0a101425928193c2cf
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-07 22:14:46 +0900

    Fix : little refactor

--- eatgo-api/src/main/java/kr/co/eatgo/domain/MenuItemRepositoryImpl.java
+++ eatgo-api/src/main/java/kr/co/eatgo/domain/MenuItemRepositoryImpl.java
@@ -1,16 +1,20 @@
 package kr.co.eatgo.domain;
 
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class MenuItemRepositoryImpl implements MenuItemRepository {
-    List<MenuItem> menuItems = new ArrayList<>();
-    MenuItemRepositoryImpl(){
+    private List<MenuItem> menuItems = new ArrayList<>();
+
+    public MenuItemRepositoryImpl(){
         menuItems.add(new MenuItem("Kimchi"));
     }
+
     @Override
     public List<MenuItem> findAllByRestaurantId(Long id) {
-        System.out.println(menuItems+"아ㅏ아아아아앙");
         return menuItems;
     }
 }
