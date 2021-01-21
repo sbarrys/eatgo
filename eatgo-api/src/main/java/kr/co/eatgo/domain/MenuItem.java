5249e09b7ba3480711dc9dee047bb60253755215
Kim Tae Yun <rlaxodbs2015@gmail.com>
2021-01-21 17:38:20 +0900

    Add  : add MenuItem  _destory field With @JsonInclude(Non_default)
     when it use?  when we get  some menuitems by responsebody ,   if it's destroy field ==TRUE   we'll delete

--- eatgo-api/src/main/java/kr/co/eatgo/domain/MenuItem.java
+++ eatgo-api/src/main/java/kr/co/eatgo/domain/MenuItem.java
@@ -1,13 +1,12 @@
 package kr.co.eatgo.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 
 @Builder
@@ -15,6 +14,7 @@ import javax.validation.constraints.NotEmpty;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MenuItem {
+    @Getter
     @Id
     @GeneratedValue
     private  Long id;
@@ -22,7 +22,13 @@ public class MenuItem {
     private  String name;
     @Setter
     private Long restaurantId;
+    @Transient
+    @JsonInclude(JsonInclude.Include.NON_DEFAULT)// boolean 의 default 값이 false 인데, 디폴트와 다르다면 넣어줘라.
+    private boolean destroy;
 
+    public Boolean isDestroy(){
+        return destroy;
+    }
     public String getName() {
         return name;
     }
